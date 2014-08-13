185c078268c67615e9d8663b3575efa4841d4178
Jon Sime <jonsime@gmail.com>
2014-08-13 13:44:24 -0400

    change package name format for install to remove warning output about publisher name mismatch

--- scripts/chef-solo.sh
+++ scripts/chef-solo.sh
@@ -1,6 +1,7 @@
 # install chef from omniti-ms repository, and symlink for easier use
 echo "Installing Chef Solo from OmniTI MS Repository"
-pkg install -g http://pkg.omniti.com/omniti-ms/ omniti/system/management/chef
+pkg install pkg://ms.omniti.com/omniti/system/management/chef
+mkdir -p /opt/omni/bin
 ln -s /opt/omni/lib/ruby/gems/1.9/bin/chef-solo /opt/omni/bin/chef-solo
 
 echo "chef-solo.sh done"
